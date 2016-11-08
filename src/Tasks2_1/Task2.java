16ca627a64470b67f7860eb82a097d47ff4a676a
Elena Verkholaz <elena.verkholaz@gmail.com>
2016-11-08 12:06:59 +0200

    Task 2 from HW2

---
+++ src/Tasks2_1/Task2.java
@@ -0,0 +1,34 @@
+package Tasks2_1;
+
+/**
+ * Created by elve on 11/8/2016.
+ */
+public class Task2 {
+    public static void main(String[] args) {
+        int b = 100;
+        int w = 10;
+        double k = 0.05;
+        op(b,w,k);
+    }
+
+
+    public static void op (int b, int w, double k){
+
+        double nb = 0;
+
+        // let's calculate commision
+        double com = w*k;
+
+        // let's calculate total withdraw with commission
+        double tw = w + com;
+
+        if (b >= tw) {
+            nb = nb + (b-tw);
+            System.out.println("OK " + com + " " + nb);
+        }
+         else {
+             System.out.println("NO");
+         }
+
+    }
+}
